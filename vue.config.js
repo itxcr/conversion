8b8edb181c335fb14fcdf9e30219c94b19dfd131
xcr <itxcr@protonmail.com>
2021-11-09 20:12:38 +0800

    替换图标

--- vue.config.js
+++ vue.config.js
@@ -16,7 +16,7 @@ module.exports = {
         asar: true,
         win: {
           // win相关配置
-          icon: 'public/favicon.ico', // 图标256*256
+          // icon: 'public/favicon.ico', // 图标256*256
           target: [
             {
               target: 'nsis',
@@ -31,9 +31,9 @@ module.exports = {
           shortcutName: 'Conversion',
           allowElevation: true, // 允许请求提升。 如果为false，则用户必须使用提升的权限重新启动安装程序。
           allowToChangeInstallationDirectory: true, // 允许修改安装目录
-          installerIcon: 'public/favicon.ico',// 安装图标
-          uninstallerIcon: 'public/favicon.ico',//卸载图标
-          installerHeaderIcon: 'public/favicon.ico', // 安装时头部图标
+          // installerIcon: 'public/favicon.ico',// 安装图标
+          // uninstallerIcon: 'public/favicon.ico',//卸载图标
+          // installerHeaderIcon: 'public/favicon.ico', // 安装时头部图标
           createDesktopShortcut: false, // 创建桌面图标
           createStartMenuShortcut: false,// 创建开始菜单图标
         },
