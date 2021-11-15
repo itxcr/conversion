d9ea90c1f162fdc3e2972030f5234124c040c8ba
xcr <itxcr@protonmail.com>
2021-11-15 18:08:48 +0800

    优化代码

--- src/renderer/views/Main.vue
+++ src/renderer/views/Main.vue
@@ -4,7 +4,7 @@
       <div class='custom-flex' style='position: fixed;z-index: 2;top: 100px;'>
         <el-input v-model.trim='value' clearable placeholder='请输入内容' style='margin-right: 10px;' />
         <el-tooltip effect='dark' content='定位小区' placement='top-start'>
-          <el-button type='plain' class='fas fa-search-location' circle size='mini' @click='dw'
+          <el-button type='plain' class='fas fa-search-location' circle size='mini' @click='searchCity'
                      :disabled='value.length===0' />
         </el-tooltip>
         <el-tooltip effect='dark' content='导出文件' placement='top-start'>
@@ -75,66 +75,57 @@ export default {
   mounted() {
     this.map = new BMap.Map('container')
     this.map.enableScrollWheelZoom(true)
-    this.dw()
+    this.searchCity()
     // console.log(this.$config)
   },
   methods: {
     //定位区域，小地名，使用本地检索方法
-    dw() {
-      this.map.clearOverlays()
+    searchCity() {
       this.coordinatesList = []
       const local = new BMap.LocalSearch(this.map, {
-        renderOptions: { map: this.map },
+        renderOptions: { map: this.map, panel: 'result' },
+        onSearchComplete: (results) => {
+          if (local.getStatus() === BMAP_STATUS_SUCCESS && results.getPoi(0)) {
+            const { uid } = results.getPoi(0)
+            return this.queryUid(uid)
+          }
+          this.$message.error('百度API搜索出错，请稍后再试！')
+        },
         pageCapacity: 1,
       })
       local.search(this.value)
-      local.setMarkersSetCallback((poi) => {
-        console.log(poi, '获取poi')
-        if (poi.length !== 0) {
-          this.map.clearOverlays()
-          //清除所有覆盖物后，在叠加第一个点
-          // this.map.clearOverlays()
-          // for (let i = 0; i < poi.length; i++) {
-          //   const marker = new BMap.Marker(poi[i].point)
-          //   this.map.addOverlay(marker)
-          // }
-          //根据获取到的poi id，查询边界坐标集合，画多边形
-          const { uid, title } = poi[0]
-          return this.queryUid(uid, title)
-        } else {
-          return this.$message.error('输入内容获取不到，请检查！')
-        }
-      })
     },
     //获取小区信息
-    async queryUid(uid, title) {
+    async queryUid(uid) {
       this.kml = []
       try {
         let url = `https://map.baidu.com/?reqflag=pcmap&from=webmap&qt=ext&uid=${uid}&ext_ver=new&l=18`
-        // let url1 = 'http://map.baidu.com/?pcevaname=pc4.1&qt=ext&ext_ver=new&l=12&uid=' + uid;
+        // 第二种根据 uid 获取小区范围坐标
+        // let url = `https://map.baidu.com/?pcevaname=mp&qt=ext&ext_ver=new&l=12&uid=${uid}`
         let arr = []
         this.coordinatesList = []
         const result = await axios.get(url)
-        console.log('通过uid 获取详细检索', result)
         let content = result.data.content
         if (content.hasOwnProperty('geo') && content.geo) {
+          this.map.clearOverlays()
           const geo = content.geo
           let points = this.coordinateToPoints(geo)
           if (points && points.indexOf(';') >= 0) {
             points = points.split(';')
           }
-          // console.log(points, 7)
-          for (let i = 0; i < points.length - 1; i++) {
+          for (let i = 0, len = points.length; i < len - 1; i++) {
             let temp = points[i].split(',')
             let convert = Conversion.BaiduToWgs84(temp[0], temp[1])
-            arr.push(new BMap.Point(parseFloat(temp[0]), parseFloat(temp[1])))
+            // 导出 84 坐标所用
             this.kml.push(convert)
+            // 地图绘制范围
+            arr.push(new BMap.Point(parseFloat(temp[0]), parseFloat(temp[1])))
             this.coordinatesList.push([parseFloat(temp[0]), parseFloat(temp[1])])
           }
-          const polygon = new BMap.Polyline(arr, { strokeColor: 'blue', strokeWeight: 2, strokeOpacity: 0.8 })  //创建多边形
+          // 地图绘制范围
+          const polygon = new BMap.Polyline(arr, { strokeColor: 'blue', strokeWeight: 2, strokeOpacity: 0.5 })  //创建多边形
           this.map.addOverlay(polygon)
           this.map.setViewport(polygon.getPath())
-          console.log(title)
           return this.tableData = this.coordinatesList.map(item => {
             return {
               id: uuidV4(),
@@ -143,50 +134,42 @@ export default {
             }
           })
         }
-        this.map.clearOverlays()
         this.tableData = []
         this.$message.error(`${this.value}  无范围可获取`)
       } catch (e) {
-        this.$message.error(`百度接口请求失败`)
+        this.$message.error(`百度API搜索出错，请稍后再试！`)
       }
     },
     //坐标转换
     coordinateToPoints(coordinate) {
-      let points = ''
       if (coordinate) {
+        let points = ''
         const projection = BMAP_NORMAL_MAP.getProjection()
-        // console.log(coordinate, '坐标转换1, 通过 uid 获取 geo')
-        // console.log(projection, '坐标转换2  BMAP_NORMAL_MAP.getProjection')
         if (coordinate && coordinate.indexOf('-') >= 0) {
-          coordinate = coordinate.split('-')
-          // console.log(coordinate, '坐标转换3 coordinate.split(\'-\')')
+          // 取点集合
+          let tempco = coordinate.split('-')[1]
+          if (tempco && tempco.indexOf(',') >= 0) {
+            tempco = tempco.replace(';', '').split(',')
+            //分割点，两个一组，组成百度米制坐标
+            const temppoints = []
+            for (let i = 0, len = tempco.length; i < len; i++) {
+              const obj = {}
+              obj.lng = tempco[i]
+              obj.lat = tempco[i + 1]
+              temppoints.push(obj)
+              i++
+            }
+            //遍历米制坐标，转换为经纬度
+            for (let i = 0, len = temppoints.length; i < len; i++) {
+              const pos = temppoints[i]
+              const point = projection.pointToLngLat(new BMap.Pixel(pos.lng, pos.lat))
+              points += ([point.lng, point.lat].toString() + ';')
+            }
+          }
         }
-        //取点集合
-        let tempco = coordinate[1]
-        // console.log(tempco, 'tempco')
-        if (tempco && tempco.indexOf(',') >= 0) {
-          tempco = tempco.replace(';', '').split(',')
-        }
-        //分割点，两个一组，组成百度米制坐标
-        const temppoints = []
-        for (let i = 0, len = tempco.length; i < len; i++) {
-          const obj = {}
-          obj.lng = tempco[i]
-          obj.lat = tempco[i + 1]
-          temppoints.push(obj)
-          i++
-        }
-        // console.log(temppoints, 4)
-        // console.log(temppoints, '5')
-        //遍历米制坐标，转换为经纬度
-        for (let i = 0, len = temppoints.length; i < len; i++) {
-          const pos = temppoints[i]
-          const point = projection.pointToLngLat(new BMap.Pixel(pos.lng, pos.lat))
-          points += ([point.lng, point.lat].toString() + ';')
-        }
+        return points
       }
-      // console.log(points, '6')
-      return points
+      this.$message.error(`百度API搜索出错，请稍后再试！`)
     },
     async exportFile() {
       try {
@@ -194,7 +177,6 @@ export default {
           title: this.value,
           geo: this.kml,
         }))
-        // console.log(result)
         if (result) {
           this.value = ''
           this.tableData = []
@@ -215,7 +197,6 @@ export default {
 
 <style scoped lang='scss'>
 ::-webkit-scrollbar {
-  /*隐藏滚轮*/
   display: none;
 }
 
